1d6bff984facce5fd688247a469289ed55291ea9
Peter H. Boling <peter.boling@gmail.com>
2026-07-07 15:42:22 -0600

    ✅ Fix version spec setup

--- spec/kettle/soup/cover/version_spec.rb
+++ spec/kettle/soup/cover/version_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "kettle/soup/cover/version_gem"
+
 RSpec.describe Kettle::Soup::Cover::Version do
   it_behaves_like "a Version module", described_class
 end
